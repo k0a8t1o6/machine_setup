a6335d08338d87af2a191a8e01ebade81b39f2ea
TakashiKato <wisy@m.atelierpal.space>
2019-02-04 21:41:02 +0900

    install several applications on ubuntu machine

---
+++ apkList.py
@@ -0,0 +1,37 @@
+# personal computer setup script for those who work with aws
+# woks on ubuntu
+
+import os
+
+
+# system programs
+apk_to_install = []
+bash_command = "sudo apt install "
+
+apk_to_install.append("python3-pip")
+apk_to_install.append("ipython3")
+apk_to_install.append("git")
+apk_to_install.append("openssh-client")
+
+
+for apk in apk_to_install:
+    print("commence installing an apk -----------------------")
+    os.system(bash_command + apk)
+    print("--------------------------------------------------")
+
+
+# programs require pip
+apk_on_pip = []
+command_pip = "pip3 install "
+
+apk_on_pip.append("boto3")
+apk_on_pip.append("tqdm")
+
+for apk in apk_on_pip:
+    print("commence installing an apk -----------------------")
+    os.system(command_pip + apk)
+    print("--------------------------------------------------")
+
+
+print("complete installing multiple programs")
+
